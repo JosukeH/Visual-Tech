3494f467c0c623476f7c84f292168cd331b73652
JosukeH <solodcarlos@gmail.com>
2022-07-04 23:41:36 -0500

    fixing bugs3

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Feature from './components/Feature'
 import Footer from './components/Footer'
 import HeroSection from './components/HeroSection'
 import NavBar from './components/NavBar'
-import Products from './components/products'
+// import Products from './components/products'
 import Team from './components/Team'
 
 function App() {
